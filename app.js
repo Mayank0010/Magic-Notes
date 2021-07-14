66d3c8203d14617573d19adb2cc7785a9c859b0f
Mayank0010 <kr.mayank.singh@gmail.com>
2021-07-14 23:27:51 +0530

    Edited app.js

--- app.js
+++ app.js
@@ -28,12 +28,11 @@ function showNotes() {
     notesObj.forEach(function(element, index) {
         html += `
       
-      <div class="card my-2 mx-2" style="width: 18rem;">
+      <div class="card my-2 mx-2 card" style="width: 18rem;">
           <div class="card-body">
               <h5 class="card-title">Note ${index + 1}</h5>
               <p class="card-text">${element}</p>
-              <button href="#" class="btn btn-danger">Delete</button>
-              <button href="#" class="btn btn-success">Update</button>
+              <button id="${index}" onclick="deleteNote(this.id)"class="btn btn-danger">Delete</button>
           </div>
   </div>`;
     });
@@ -45,3 +44,35 @@ function showNotes() {
         notesElem.innerHTML = "Nothing to show!!";
     }
 }
+
+
+
+
+function deleteNote(index) {
+    let notes = localStorage.getItem("notes");
+
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
+}
+
+let search = document.getElementById("searchTxt");
+search.addEventListener("input", function() {
+    inputVal = search.value;
+    console.log("Input event fired", inputVal);
+    let card = document.getElementsByClassName("card");
+    Array.from(card).forEach(function(element) {
+        let cardTxt = element.getElementsByTagName("p")[0].innerText;
+        if (cardTxt.includes(inputVal)) {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
+
+    })
+})
